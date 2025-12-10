ddae426a49f07c7aae3a06ed911e60bbb8046fa6
Peter Kovacs <wi24b183@technikum-wien.at>
2025-12-10 14:43:54 +0100

    Project Tree updated

---
+++ src/main/java/org/example/ElectricChargingStationNetwork.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ElectricChargingStationNetwork {
+    public static void main(String[] args) {
+        System.out.println("Electric Chargin Station Network");
+    }
+}
